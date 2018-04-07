860c3f3d1d33ce75cd3f945a6db978e07b5743e6
Jared Wines <jwines760@gmail.com>
2018-04-07 09:33:50 -0700

    Add garage, room1 and nav drawers

--- Andriod/app/src/main/java/com/jdwstudio/jared/smartroom/MainActivity.java
+++ Andriod/app/src/main/java/com/jdwstudio/jared/smartroom/MainActivity.java
@@ -2,9 +2,6 @@ package com.jdwstudio.jared.smartroom;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -47,7 +44,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.settings, menu);
         return true;
     }
 
@@ -71,15 +68,17 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_gate) {
-            Intent i = new Intent(getApplicationContext(),GateOpener.class);
+        if (id == R.id.nav_main) {
+            //Is already in MainActivity.class
+            //Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            //startActivity(i);
+        } else if (id == R.id.nav_room1) {
+            Intent i = new Intent(getApplicationContext(),Room1Activity.class);
             startActivity(i);
-        } else if (id == R.id.nav_tv) {
-
+        } else if (id == R.id.nav_gate) {
+            Intent i = new Intent(getApplicationContext(),GateOpenerActivity.class);
+            startActivity(i);
         } else if (id == R.id.nav_garage) {
-
-        } else if (id == R.id.nav_lights_fan) {
 
         } else if (id == R.id.nav_about) {
 
