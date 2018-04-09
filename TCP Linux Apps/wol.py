a999362eb81cfab8a7c2bd5581c8df8a24b289cc
Jared Wines <jwines760@gmail.com>
2018-04-08 22:11:05 -0700

    reorganize repository

--- TCP Linux Apps/wol.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/python3
-import socket
-
-s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.sendto(('\xff'*6+'\xbc\x5f\xf4\x47\x2b\xbb'*16).encode('utf-8'), ("192.168.1.255",9))
