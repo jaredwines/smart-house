8819a4d1b6166f979c4eace30084049da9a70aed
Jared Wines <jwines760@gmail.com>
2018-04-08 22:19:48 -0700

    Rename and reorganize repository

---
+++ Android/app/src/main/java/com/jdwstudio/jared/smartroom/RoomActivity.java
@@ -0,0 +1,328 @@
+package com.jdwstudio.jared.smartroom;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RoomActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private TCPClient mTcpClient;
+    private connectTask cTask;
+    Button TvPower, TvSource, SoundBarPower,SoundBarSouce,SoundBarVolDown,SoundBarVolUp,SoundBarMute,
+            SoundBarPre,SoundBarPlay,SoundBarNext,LightsOn,LightsOff,FanOn,FanOff,PCWol;
+    TextView ConnectionInfo, Warning;
+    String host = "pi.jaredwines.com";
+    int port = 8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //call the widgets
+        TvPower = (Button) findViewById(R.id.tv_power);
+        TvSource = (Button) findViewById(R.id.tv_source);
+        SoundBarPower = (Button) findViewById(R.id.soundbar_power);
+        SoundBarSouce = (Button) findViewById(R.id.soundbar_source);
+        SoundBarVolDown = (Button) findViewById(R.id.soundbar_vol_down);
+        SoundBarVolUp = (Button) findViewById(R.id.soundbar_vol_up);
+        SoundBarMute = (Button) findViewById(R.id.soundbar_mute);
+        SoundBarPre = (Button) findViewById(R.id.soundbar_pre);
+        SoundBarPlay = (Button) findViewById(R.id.soundbar_play);
+        SoundBarNext = (Button) findViewById(R.id.soundbar_next);
+        LightsOn = (Button) findViewById(R.id.lights_on);
+        LightsOff = (Button) findViewById(R.id.lights_off);
+        FanOn = (Button) findViewById(R.id.fan_on);
+        FanOff = (Button) findViewById(R.id.fan_off);
+        PCWol = (Button) findViewById(R.id.pc_wol);
+        ConnectionInfo = (TextView) findViewById(R.id.connectionInfo);
+        Warning = (TextView) findViewById(R.id.warning);
+
+        cTask = new connectTask();
+        cTask.execute();
+
+        TvPower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote tv power");
+            }
+        });
+        TvSource.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote tv source");
+            }
+        });
+        SoundBarPower.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar power");
+            }
+        });
+        SoundBarSouce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar source");
+            }
+        });
+        SoundBarVolDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar voldown");
+            }
+        });
+        SoundBarVolUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar volup");
+            }
+        });
+        SoundBarMute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar mute");
+            }
+        });
+        SoundBarPre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar pre");
+            }
+        });
+        SoundBarPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar play");
+            }
+        });
+        SoundBarNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("remote soundbar next");
+            }
+        });
+        LightsOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("lights on");
+            }
+        });
+        LightsOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("lights off");
+            }
+        });
+        FanOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("fan on");
+            }
+        });
+        FanOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("fan off");
+            }
+        });
+        PCWol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommandToServer("pc wol");
+            }
+        });
+    }
+
+    public class connectTask extends AsyncTask<String, String, TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            mTcpClient = new TCPClient(new TCPClient.TcpClientListener() {
+                @Override
+                public void onMessageReceived(TCPClient tcpClient, String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+
+                @Override
+                public void onConnecting(TCPClient tcpClient, String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+
+                @Override
+                public void onConnected(TCPClient tcpClient, String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+
+                @Override
+                public void onDisconnected(TCPClient tcpClient, String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+
+            });
+            mTcpClient.start(host, port);
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            /*
+            2 = connecting to server
+            3 = connected to server
+            4 = disconnected from server
+             */
+
+            //Connected to server
+            if (values[0].equals("3")) {
+                //Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+                ConnectionInfo.setTextColor(Color.GREEN);
+                ConnectionInfo.setText("Connected");
+                Warning.setText("");
+            }
+            //Disconnected from server
+            else if (values[0].equals("4")) {
+                //Toast.makeText(getApplicationContext(), "Disconnected", Toast.LENGTH_LONG).show();
+                if(isCorrectSSID()) {
+                    ConnectionInfo.setTextColor(Color.RED);
+                    ConnectionInfo.setText("Disconnected");
+                    Warning.setText("");
+                }
+                else {
+                    ConnectionInfo.setTextColor(Color.RED);
+                    ConnectionInfo.setText("Error!!!");
+                    Warning.setText("You are not connected to network \"Wines\"!");
+                }
+            }
+        }
+    }
+
+    private void connect() {
+        if(isCorrectSSID())
+        {
+            if (mTcpClient.isConnected())
+                Toast.makeText(getApplicationContext(), "You are already connected to the server.", Toast.LENGTH_LONG).show();
+            else {
+                mTcpClient.start(host, port);
+            }
+        }
+        else
+            Toast.makeText(getApplicationContext(), "You are not connected to network \"Wines\"!", Toast.LENGTH_LONG).show();
+    }
+
+    private void sendCommandToServer(String command)
+    {
+        if (mTcpClient.isConnected()) {
+            //sends the message to the server
+            if (mTcpClient != null) {
+                mTcpClient.sendMessage(command);
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Error! Try pressing \"CONNECT\" and make sure your connected to wifi named \"Wines\".", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isCorrectSSID()
+    {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        if (wifiInfo.getSSID().equals("\"Wines\""))
+            return true;
+        else
+            //return false;
+            return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.room, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_connect) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_main) {
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_gate_opener) {
+            Intent i = new Intent(getApplicationContext(),GateOpenerActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_garage_opener) {
+            Intent i = new Intent(getApplicationContext(),GarageOpenerActivity.class);
+            startActivity(i);
+        } else if (id == R.id.nav_room) {
+            //Is already in RoomActivity.class
+            //Intent i = new Intent(getApplicationContext(),RoomActivity.class);
+            //startActivity(i);
+        } else if (id == R.id.nav_about) {
+            Intent i = new Intent(getApplicationContext(),AboutActivity.class);
+            startActivity(i);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ Linux Apps/remote.c
@@ -0,0 +1,108 @@
+#include<string.h>
+#include<stdio.h>		//printf
+#include<string.h>		//strlen
+#include<stdlib.h> 		//system()
+
+int main (int argc, char *argv[])
+{
+
+	if (argc != 3)
+    {
+      fprintf (stderr, "Error: ./remote <device> <function> \n");
+      return 1;
+    }
+	
+	char * arrOfTVCommands[] = 
+	{
+		  "irsend SEND_ONCE TV KEY_POWER", 			//index 0
+		  "irsend SEND_ONCE TV KEY_VOLUMEUP", 		//index 1
+		  "irsend SEND_ONCE TV KEY_VOLUMEDOWN"		//index 2
+	};
+	
+	char * arrOfSoundBarCommands[] = 
+	{
+		"irsend SEND_ONCE SOUNDBAR KEY_POWER", 		//index 0
+		"irsend SEND_ONCE SOUNDBAR KEY_VOLUMEUP", 	//index 1
+		"irsend SEND_ONCE SOUNDBAR KEY_VOLUMEDOWN", //index 2
+		"irsend SEND_ONCE SOUNDBAR KEY_S", 			//index 3
+		"irsend SEND_ONCE SOUNDBAR KEY_MUTE", 		//index 4
+		"irsend SEND_ONCE SOUNDBAR KEY_LEFT", 		//index 5
+		"irsend SEND_ONCE SOUNDBAR KEY_RIGHT", 		//index 6
+		"irsend SEND_ONCE SOUNDBAR KEY_NEXT", 		//index 7
+		"irsend SEND_ONCE SOUNDBAR KEY_PLAY", 		//index 8
+		"irsend SEND_ONCE SOUNDBAR KEY_PREVIOUS",	//index 9
+		"irsend SEND_ONCE SOUNDBAR KEY_SHUFFLE", 	//index 10
+		"irsend SEND_ONCE SOUNDBAR KEY_FN1", 		//index 11
+		"irsend SEND_ONCE SOUNDBAR KEY_FN2", 		//index 12
+		"irsend SEND_ONCE SOUNDBAR KEY_FN3", 		//index 13
+		"irsend SEND_ONCE SOUNDBAR KEY_FN3"			//index 14
+	};
+	
+	if (strcmp(argv[1], "tv") == 0)
+	{
+		if (strcmp(argv[2], "power") == 0)
+			system(arrOfTVCommands[0]);
+		
+		else if (strcmp(argv[2], "volup") == 0)
+			system(arrOfTVCommands[1]);
+		
+		else if (strcmp(argv[2], "voldown") == 0)
+			system(arrOfTVCommands[2]);
+		else
+			printf("Error: Invalid Function Name!\n");  
+	}
+	
+	else if (strcmp(argv[1], "soundbar") == 0)
+	{
+		if (strcmp(argv[2], "power") == 0)
+			system(arrOfSoundBarCommands[0]);
+		
+		else if (strcmp(argv[2], "volup") == 0)
+			system(arrOfSoundBarCommands[1]);
+		
+		else if (strcmp(argv[2], "voldown") == 0)
+			system(arrOfSoundBarCommands[2]);
+		
+		else if (strcmp(argv[2], "source") == 0)
+			system(arrOfSoundBarCommands[3]);
+		
+		else if (strcmp(argv[2], "mute") == 0)
+			system(arrOfSoundBarCommands[4]);
+		
+		else if (strcmp(argv[2], "left") == 0)
+			system(arrOfSoundBarCommands[5]);
+		
+		else if (strcmp(argv[2], "right") == 0)
+			system(arrOfSoundBarCommands[6]);
+		
+		else if (strcmp(argv[2], "next") == 0)
+			system(arrOfSoundBarCommands[7]);
+		
+		else if (strcmp(argv[2], "play") == 0)
+			system(arrOfSoundBarCommands[8]);
+		
+		else if (strcmp(argv[2], "pre") == 0)
+			system(arrOfSoundBarCommands[9]);
+		
+		else if (strcmp(argv[2], "effect") == 0)
+			system(arrOfSoundBarCommands[10]);
+		
+		else if (strcmp(argv[2], "sound") == 0)
+			system(arrOfSoundBarCommands[11]);
+		
+		else if (strcmp(argv[2], "bluetooth") == 0)
+			system(arrOfSoundBarCommands[12]);
+		
+		else if (strcmp(argv[2], "control") == 0)
+			system(arrOfSoundBarCommands[13]);
+		else
+			printf("Error: Invalid Function Name!\n");  
+	}
+	
+	else
+		printf("Error: Invalid Device Name!\n");  
+	
+	
+	return 0;
+}
+
